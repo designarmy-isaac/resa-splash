6f120d384cf1e4a50d82c9215356904ff399f745
Nick DiMatteo <hello@nickdimatteo.com>
2016-05-19 17:20:13 -0400

    begin content addition

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+function showContent(sectionIndex) {
+  //bring in content bands
+  $('body').addClass('section-open');
+
+  // set active section item
+  $('.sections .band').removeClass('active');
+  $('.sections').each(function() {
+    $('.band', this).eq(sectionIndex).addClass('active');
+  });
+
+  // set active page content
+  $('.pages .band .content').removeClass('active');
+  $('.pages .band').each(function() {
+    $('.content', this).eq(sectionIndex).addClass('active');
+  });
+}
+
 $(window).load(function() {
   $('.loader').velocity({
     opacity: 0
@@ -7,4 +24,22 @@ $(window).load(function() {
       $('body').addClass('loaded');
     }
   });
+});
+
+$('a.section-link').on('click', function(e) {
+  e.preventDefault();
+  var sectionIndex = $(this).parent().index();
+
+  // scroll to top if on mobile
+  if($(window).width() < 768) {
+    $('.header').velocity('scroll', {
+      duration: 400,
+      easing: 'easeInOutExpo',
+      complete: function() {
+        showContent(sectionIndex);
+      }
+    });
+  } else {
+    showContent(sectionIndex);
+  }
 });
