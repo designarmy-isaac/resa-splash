f409f51f653e8acb931b529fdfa475b1397da476
Nick DiMatteo <hello@nickdimatteo.com>
2016-05-19 14:02:33 -0400

    rely on velocity for delay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,10 @@
 $(window).load(function() {
   $('.loader').velocity({
-      opacity: 0
-    }, 400, function() {
+    opacity: 0
+  }, {
+    delay: 1000,
+    complete: function() {
       $('body').addClass('loaded');
-    });
-  // setTimeout(function() {
-  //   $('.loader').velocity({
-  //     opacity: 0
-  //   }, 400, function() {
-  //     $('body').addClass('loaded');
-  //   });
-  // }, 1000);
+    }
+  });
 });
